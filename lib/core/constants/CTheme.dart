4de8a1f196c74c62e6675fcad0e0d370e82d23cb
GHOST <adarshkakkat7@gmail.com>
2021-07-09 15:48:18 +0530

    changed font size and weight for errors

--- lib/core/constants/CTheme.dart
+++ lib/core/constants/CTheme.dart
@@ -2,6 +2,6 @@ import 'package:flutter/material.dart';
 import 'CColor.dart';
 class CTheme{
   static ThemeData get lightTheme{
-    return ThemeData(primaryColor:CColor.PrimaryColor,backgroundColor: CColor.HomeScreenBGColor,textTheme: TextTheme(headline1: TextStyle(fontFamily:'Ubuntu',fontSize: 15,color:CColor.HomeScreenBGColor,fontWeight: FontWeight.w500),headline2: TextStyle(fontFamily:'Roboto',color:CColor.TextBlack,fontSize:15,fontWeight: FontWeight.w700), ));
+    return ThemeData(primaryColor:CColor.PrimaryColor,backgroundColor: CColor.HomeScreenBGColor,textTheme: TextTheme(headline1: TextStyle(fontFamily:'Ubuntu',fontSize: 15,color:CColor.HomeScreenBGColor,fontWeight: FontWeight.w500),headline2: TextStyle(fontFamily:'Roboto',color:CColor.TextBlack,fontSize:20,fontWeight: FontWeight.w500), ));
   }
 }
